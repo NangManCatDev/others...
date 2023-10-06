f733d52fe57eae7481ffbea69c88529b6bc75582
NangManCatDev <110078571+NangManCatDev@users.noreply.github.com>
2023-10-06 14:59:03 +0900

    Add files via upload

---
+++ 2023_10_06/GooGoo.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class GooGoo{
+    public static void main(String[] args) {
+        GooGoo goo = new GooGoo();
+        GooGooThread t = goo.new GooGooThread();
+        t.start();
+    }
+
+    class GooGooThread extends Thread {
+        public void run() {
+            System.out.print("Execute Thread. input the number you want to calculate : _\b");
+            Scanner scanner = new Scanner(System.in);
+            boolean isValid = false;
+            
+            while (!isValid) {
+                int n = scanner.nextInt();
+                if (n > 9 || n <= 0) {
+                    isValid = false;
+                    System.out.print("Invalid input. input the number again : _\b");
+                }
+                else {
+                    System.out.println("Calculating . . . ");
+                    for (int i = 1; i <= 9; i++) {
+                        System.out.println(n + " * " + i + " = " + (n * i));
+                    }
+                    System.out.print("calculate has been completed.");
+                    isValid = true;
+                }
+            }
+            
+            scanner.close();
+        }
+    }
+}
